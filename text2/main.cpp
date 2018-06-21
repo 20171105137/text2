3e2c1c813dc8e7744080f33bd61af3e50cbba98f
20171105137 <1124891566@qq.com>
2018-06-21 09:17:03 +0800

    Initial Commit

---
+++ text2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text2
+//
+//  Created by s20171105137 on 2018/6/21.
+//  Copyright © 2018年 s20171105137. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
